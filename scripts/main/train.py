57235e126dcda9c246c48395077091d4a6af97b5
Dmitry Savkin <dmitry.savkin@student.htw-berlin.de>
2022-03-20 20:34:01 +0100

    work version

--- scripts/main/train.py
+++ scripts/main/train.py
@@ -8,6 +8,7 @@ if gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        input("Ready")
     except RuntimeError as e:
         # Memory growth must be set before GPUs have been initialized
         print(e)
